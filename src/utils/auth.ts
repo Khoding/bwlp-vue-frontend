c6235884492b7abbd67c8c1870c9a305e42850b6
Lukas Berger <lukas@grovia.de>
2025-02-22 14:31:22 +0100

    builder pattern and sanity checks

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
 "use-strict"
 
 type AuthProps = {
-    AuthServerBaseURL: string,
+    AuthServerBaseURL?: string,
     AuthPath?: string,
     RedirectURL?: string,
     Token?: string,
@@ -10,12 +10,15 @@ type AuthProps = {
 export class Auth {
     private authServerBaseURL: string
     private authPath: string
-    private redirectURL?: string
+    private redirectURL: string
     private entityID: string
+    private token: string
 
     constructor(authProps: AuthProps) {
-        this.authServerBaseURL = authProps.AuthServerBaseURL
-        this.authPath = authProps.AuthPath
+        this.authServerBaseURL = authProps.AuthServerBaseURL ?? ""
+        this.authPath = authProps.AuthPath ?? ""
+        this.redirectURL = authProps.RedirectURL ?? ""
+        this.token = authProps.Token ?? ""
     }
 
 
@@ -24,15 +27,24 @@ export class Auth {
     }
 
     public generateLoginURL(): string {
+
+        if (!this.redirectURL || this.redirectURL === "") {
+            throw new Error("No redirect given")
+        }
+        if (!this.entityID || this.entityID === "") {
+            throw new Error("No entityId was given")
+        }
+        if (!this.authServerBaseURL || this.authServerBaseURL === "") {
+            throw new Error("No base url for auth was set")
+        }
+        if (!this.authPath || this.authPath === "") {
+            throw new Error("No url path to auth was set")
+        }
+
         const base = "https://" + this.authServerBaseURL
         const authPath = base + this.authPath
 
-        if (!this.redirectURL) {
-            throw new Error("No redirect given")
-        }
-        if (!this.entityID) {
-            throw new Error("No entityId was given")
-        }
+        
 
         const entityIDEncoded = encodeURIComponent(this.entityID)
         const redirectEncoded = encodeURIComponent(this.redirectURL)
